32d1147a3d073c5303f516913818f80e2f545a76
Richard Flores <lcdo.richardflores@gmail.com>
2024-10-23 22:36:38 -0300

    vuelve a funcionar filtrado por categorias

--- Proyecto-Final-React-Flores/src/Components/ItemListContainer/ItemListContainer.jsx
+++ Proyecto-Final-React-Flores/src/Components/ItemListContainer/ItemListContainer.jsx
@@ -3,18 +3,20 @@ import CardItems from "../ItemList/CardItems";
 import Container from "react-bootstrap/Container";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase/Config";
 
 const ItemListContainer = () => {
 	const [products, setProducts] = useState([]);
 
 	const category = useParams().category;
-	console.log(category);
 
 	useEffect(() => {
 		const productsRef = collection(db, "items");
-		getDocs(productsRef).then((res) => {
+		const q = category
+			? query(productsRef, where("category", "==", category))
+			: productsRef;
+		getDocs(q).then((res) => {
 			setProducts(
 				res.docs.map((product) => {
 					return { ...product.data(), id: product.id };
