b5f3e268ee0c7ea13e527e1ed087fbf5adaf0d06
Richard Flores <lcdo.richardflores@gmail.com>
2024-10-26 23:32:06 -0300

    mejoranco conexion firebase

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,35 +1,22 @@
 import { useParams } from "react-router-dom";
-import Spinner from "../../reestructuracion/Loader/Loading";
+// import Spinner from "../../reestructuracion/Loader/Loading";
 import ItemDetail from "../../Components/ItemDetail/ItemDetail";
 import { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../reestructuracion/firebase/Config";
-import { requestItemId } from "../helper/GetData";
+import { db } from "../../firebase/Config";
 
 const ItemDetailContainer = () => {
 	const [item, setItem] = useState(null);
 	const id = useParams().id;
 
 	useEffect(() => {
-		requestItemId(Number(id)).then((res) => {
-			setItem(res);
+		const docRef = doc(db, "items", id);
+		getDoc(docRef).then((resp) => {
+			setItem({ ...resp.data(), id: resp.id });
 		});
 	}, [id]);
 
 	return <>{item && <ItemDetail item={item} />}</>;
 };
 
-// const ItemDetailContainer = () => {
-// 	const [item, setItem] = useState(null);
-// 	const id = useParams().id;
-
-// 	useEffect(() => {
-// 		const docRef = doc(db, "items", id);
-// 		getDoc(docRef).then((resp) => {
-// 			setItem({ ...resp.data(), id: resp.id });
-// 		});
-// 	}, [id]);
-
-// 	return <>{item && <ItemDetail {...item} />}</>;
-// };
 export default ItemDetailContainer;
--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,11 +1,11 @@
 import { Row } from "react-bootstrap";
 import Item from "./Item";
 
-const ItemList = ({ products, title }) => {
+const ItemList = ({ products, category }) => {
 	return (
 		<>
 			<Row className="titulo-tienda">
-				<h2>Nuestros Productos: {title}</h2>
+				<h2>Nuestros Productos: {category}</h2>
 			</Row>
 			<Row xs="4">
 				{products.map((products) => (
